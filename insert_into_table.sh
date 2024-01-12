b9d60bea38e66cb656f9bad8e65e4c5480e86cbf
Esraa9955 <abdelmaksoudesraa99@gmail.com>
2024-01-13 01:30:30 +0200

    add validation to ensure that the 'id' column is unique and not repeated

--- insert_into_table.sh
+++ insert_into_table.sh
@@ -1,12 +1,14 @@
 #! /usr/bin/bash
+echo  -e "${note} Database ${dbname} Tables....."
+ls  ${path}/${dbname}
+echo -e "${base} ${NC}" 
 #prompt the user to enter the table name
 read -p "Enter the table name: " tablename
+tablename=$(echo "$tablename" | tr ' ' '_')
 #check if the table file exists
-
-if [ ! -f "$path/$dbname/$tablename" ] ;
-then
-echo -e "${invalid}Table $tablename does not exist. Exiting....${NC} "
-#exit 1
+if [[ -z $tablename || ! -f $path/$dbname/$tablename ]] ; then
+echo -e "${invalid} The Table ${tablename} NOt Found!${NC}"
+source db_menu.sh
 fi
 # get column names and datatypes from the table file
 column_names=$(sed -n '1p' "$path/$dbname/$tablename")
@@ -19,7 +21,21 @@ IFS='|' read -ra types_array <<< "$column_types"
 
 for((i=0;i<${#names_array[@]};i++)) ;
 do
+# check if the column is PK (primary key)
+if [ "${names_array[$i]}" == "id" ]; then
+while true; do
+read -p "Enter data for ${names_array[$i]} (${types_array[$i]}): " value 
+ # check if the entered id already exists in the table
+if ! grep -q "|$value|" "$path/$dbname/$tablename"; then
+break
+else
+echo -e "${invalid}The entered id $value already exists. Please enter a unique id.${NC}"
+fi
+done
+else
+# for non-id columns, proceed normally
 read -p "Enter data for ${names_array[$i]} (${types_array[$i]}): " value
+fi
 data+="$value|"
 done
 echo -e "$data"
@@ -29,5 +45,7 @@ echo $data >> $path/$dbname/$tablename
 #echo -e "$path/$dbname/$tablename"
 
 echo -e "${note} Data inserted successfully!${NC}"
+source db_menu.sh
+
 source master_menu.sh
-#source creat_table.sh
+
